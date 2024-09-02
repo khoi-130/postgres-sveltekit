b29adc3e0261340ae0e8bff82921eb00e7bf7bf1
khoi-130 <ngokhoianh2003@gmail.com>
2024-09-02 03:21:00 -0400

    Test bom postgres

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
     href="https://vercel.com"
     class="px-6 py-2 text-sm font-medium text-gray-600 transition-all rounded-full shadow-sm bg-white/30 dark:bg-white/80 ring-1 ring-gray-900/5 dark:text-black hover:shadow-lg active:shadow-sm"
   >
-    Deploy your own to Vercel
+    BOM VERCEL POSTGRES
   </a>
   <h1
     class="pt-4 pb-8 bg-gradient-to-br dark:from-white from-black via-[#707070] to-[#ffffff] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
